f8cfd5f587679878c912dcae8ad8b13a435db3e4
Patryk Milewski <mpatryk97@gmail.com>
2025-07-30 04:30:36 +0200

    feat(services): pure validator and path converter

---
+++ FeatureFlagsAPI/Services/PathConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace FeatureFlagsAPI.Services;
+
+public static class PathConverter
+{
+    public static string ToName(string path)
+    {
+        var sb = new StringBuilder("Get".Length + path.Length)
+            .Append("Get");
+
+        for (int i = 0; i < path.Length; ++i)
+        {
+            char c = path[i];
+            bool isValid = c is not '/' and not '{' and not '}';
+
+            if (i is 0 || isValid && path[i - 1] is '/' or '{' or '}')
+                sb.Append(char.ToUpper(c));
+            else if (isValid)
+                sb.Append(c);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ FeatureFlagsAPI/Services/Validator.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+using FeatureFlagsAPI.Models;
+
+namespace FeatureFlagsAPI.Services;
+
+public static class Validator
+{
+    public static IDictionary<string, string[]> Validate(
+            (Func<bool> condition, string errorMessage)[] conditions,
+            [CallerMemberName] string propName = ""
+        )
+    {
+        var validator = new Dictionary<string, string[]>();
+        var errors = new List<string>();
+        foreach ((Func<bool> condition, string errorMessage) in conditions) 
+            try
+            {
+                if (condition())
+                    errors.Add(errorMessage);
+            }
+            catch (Exception ex)
+            {
+                errors.Add(Debugger.IsAttached ? ex.Message : "Unexpected error occured.");
+            }
+        if (errors.Count > 0)
+            validator[propName] = errors.ToArray();
+        return validator;
+    }
+
+    public static bool IsTimeInSecondsValid(int seconds) =>
+        seconds >= 0;
+
+    public static bool IsIdValid(string id) =>
+        !string.IsNullOrWhiteSpace(id);
+
+    public static bool IsIdValid(int id) => 
+        id > 0;
+
+    public static bool IsFeatureFlagValid(FeatureFlag flag) =>
+        flag is not null and { Id: >= 0 } && IsFeatureFlagNameValid(flag.Name);
+
+    public static bool IsFeatureFlagNameValid(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            return false;
+        bool isCorrect = true;
+        for (int i = 0; i < name.Length; i++)
+            isCorrect &= char.IsLetterOrDigit(name[i]);
+        return isCorrect;
+    }
+
+    public static bool IsUserValid(User user) =>
+        user is not null && IsUserNameValid(user.Name);
+
+    public static bool IsUserNameValid(string name) =>
+        name?.Trim() is { Length: > 0 };
+}
