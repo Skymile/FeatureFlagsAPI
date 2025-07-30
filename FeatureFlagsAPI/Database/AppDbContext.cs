4d1a58ca3ed6d079cf80e8302db302f689fc7b01
Patryk Milewski <mpatryk97@gmail.com>
2025-07-30 04:32:28 +0200

    feat(db): in-memory db and data loader

---
+++ FeatureFlagsAPI/Database/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FeatureFlagsAPI.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace FeatureFlagsAPI.Database;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<FeatureFlag> FeatureFlags { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder
+        .Entity<FeatureFlag>()
+            .HasMany(i => i.Dependant)
+            .WithMany()
+            .UsingEntity<Dictionary<string, FeatureFlag>>(
+                "FeatureFlagsLink",
+                j => j.HasOne<FeatureFlag>().WithMany().HasForeignKey("ChildId").OnDelete(DeleteBehavior.Restrict),
+                j => j.HasOne<FeatureFlag>().WithMany().HasForeignKey("ParentId").OnDelete(DeleteBehavior.Restrict)
+            );
+}
---
+++ FeatureFlagsAPI/Database/DataLoader.cs
@@ -0,0 +1,60 @@
+﻿using FeatureFlagsAPI.Models;
+
+namespace FeatureFlagsAPI.Database;
+
+public static class DataLoader
+{
+    public static AppDbContext Load(AppDbContext context)
+    {
+        var read = FeatureFlag.Create("Read");
+        read.IsActive = true;
+
+        var write = FeatureFlag.Create("Write")
+            .AddDependant(read);
+        write.IsActive = true;
+
+        var delete = FeatureFlag.Create("Delete")
+            .AddDependant(read)
+            .AddDependant(write);
+
+        var create = FeatureFlag.Create("Create")
+            .AddDependant(read)
+            .AddDependant(write);
+        create.IsActive = true;
+
+        var dropDatabase = FeatureFlag.Create("DropDatabase");
+
+        var fullRights = FeatureFlag.Create("FullRights")
+            .AddDependant(read)
+            .AddDependant(write)
+            .AddDependant(delete)
+            .AddDependant(create)
+            .AddDependant(dropDatabase);
+        fullRights.IsActive = true;
+
+
+        var bannedUser = User.Create("Banned Sample User");
+
+        var guestUser = User.Create("Guest Sample User")
+            .AddCapability(read);
+        
+        var normalUser = User.Create("Normal Sample User")
+            .AddCapability(read)
+            .AddCapability(write);
+        
+        var modUser = User.Create("Mod Sample User")
+            .AddCapability(delete)
+            .AddCapability(create);
+        
+        var adminUser = User.Create("Admin Sample User")
+            .AddCapability(fullRights);
+
+        
+        context.Users.AddRange(
+            bannedUser, guestUser, normalUser, modUser, adminUser);
+        context.FeatureFlags.AddRange(
+            read, write, delete, create, dropDatabase, fullRights);
+
+        return context;
+    }
+}
